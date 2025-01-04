6dc9c90d32d671fa5c3e30e7e4be25b4204aa67a
popov_valeriy <popovvaleriy34@gmail.com>
2025-01-04 22:49:35 +0300

    Django 0.2

---
+++ first_project/first/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index_page(request):
+    context = {}
+    context["author"] = "Валера"
+    context["pages"] = 1
+
+    return render(request, 'index.html', context)
--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.template.defaultfilters import first
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'first/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
