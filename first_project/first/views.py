455b604657155a4e11de37606462c7c24ce31f63
popov_valeriy <popovvaleriy34@gmail.com>
2025-01-04 23:01:42 +0300

    Django 0.4

--- first_project/first/views.py
+++ first_project/first/views.py
@@ -5,7 +5,7 @@ import datetime
 def index_page(request):
     context = {}
     context["author"] = "Валера"
-    context["pages"] = 2
+    context["pages"] = 3
 
     return render(request, 'index.html', context)
 
@@ -19,3 +19,15 @@ def time_page(request):
     context["year"] = datetime.datetime.now().date().year
 
     return render(request, 'time.html', context)
+
+def calc_page(request):
+    context = {}
+    a = request.GET.get("a", 0)
+    b = request.GET.get("b", 0)
+    if a == "":
+        a = 0
+    if b == "":
+        b = 0
+    c = int(a) + int(b)
+    context["summ"] = c
+    return render(request, 'calc.html', context)
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -22,6 +22,6 @@ from first import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_page),
-    path('time/', views.time_page)
-
+    path('time/', views.time_page),
+    path('calc/', views.calc_page)
 ]
